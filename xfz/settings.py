df01d71581ef682a06013562fafe1fd2665a2954
yiyuanyan <yiyuanhanly@126.com>
2019-07-27 00:38:36 +0800

    tttt

--- xfz/settings.py
+++ xfz/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'xfz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'front')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
